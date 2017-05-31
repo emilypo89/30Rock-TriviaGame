ff88dd60c204e4bd2265cdba0ebb222f7990ee00
emilypo89 <emilypo89@gmail.com>
2017-05-31 18:14:07 -0400

    working countdown

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,57 +1,31 @@
+
+
 window.onload = function() {
-// on click timer starts
-// displays questions page
-$("#start").click(gameTimer.start);
-$("#submit").click(gameTimer.stop);
+  $("#start").click(run);
+  $("#submit").click(stop);
+  $("#questionSheet").hide();
 };
 
-// variable storing the setTimout function to begin the game
+var number = 100;
 var startGame;
-var questionsDisplayed = false;
+var displayQuestion = false;
 
-var gameTimer = {
-
-  time: 0,
-  reset: function() {
-
-    stopwatch.time = 0;
-    $("#countDown").html("00:00");
-  },
-
-  start: function() {
-      if (questionsDisplayed == false) {
-        startGame = setInterval(gameTimer.count, 20000);
-        questionsDisplayed = true;
-      }
-
-  },
-  stop: function() {
-    clearInterval(startGame);
-    questionsDisplayed = false;
-
-  },
-
-  count: function() {
-    gameTimer.time++;
-    var converted = gameTimer.timeConverter(gameTimer.time);
-    console.log(converted);
-    $("#display").html(converted);
-  },
-
-  timeConverter: function(t) {
-    var minutes = Math.floor(t / 60);
-    var seconds = t - (minutes * 60);
-    if (seconds < 10) {
-      seconds = "0" + seconds;
+function run() {
+  startGame = setInterval(decrement, 1000);
     }
-    if (minutes === 0) {
-      minutes = "00";
-    }
-    else if (minutes < 10) {
-      minutes = "0" + minutes;
-    }
-    return minutes + ":" + seconds;
+
+function decrement() {
+  number--;
+  $("#show-number").html("<h2>" + number + "</h2>");
+  if (number === 0) {
+    stop();
+    alert("Time Up!");
   }
+}
 
+function stop() {
+      clearInterval(startGame);
 
-};
+    }
+
+run();
