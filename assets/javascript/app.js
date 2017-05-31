c5a1c7fb658691bc7441fe9a72caaa0fa8b95a75
emilypo89 <emilypo89@gmail.com>
2017-05-31 19:02:03 -0400

    hide/show buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 // starts when page loads
 window.onload = function() {
   $("#button1").click(run);
-  $("#button1").html("Start!")
+  $("#button1").html("Push to Start the Game!")
   $("#submit").click(stop);
   $("#questionSheet").hide();
   $("#button2").hide();
@@ -14,11 +14,18 @@ window.onload = function() {
 var number = 100;
 var startGame;
 var displayQuestion = false;
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var noResponse = 10;
 
 // countdown functions
 function run() {
   startGame = setInterval(decrement, 1000);
-  $("#questionSheet").show();
+  $("#button2").show();
+  $("#button3").show();
+  $("#button4").show();
+  // add here the first question added to id question and the answers within the buttons
+
 }
 
 function decrement() {
@@ -31,8 +38,12 @@ function decrement() {
 }
 
 function stop() {
-      clearInterval(startGame);
-
+  clearInterval(startGame);
+  $("#button1").hide();  
+  $("#button2").hide();
+  $("#button3").hide();
+  $("#button4").hide();
+  // add here the score stats display in the question div!
+  // maybe have a restart function
     }
 
-// run();
