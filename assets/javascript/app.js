14c051a92b4e610479050407ec4c94066d9d3b08
emilypo89 <emilypo89@gmail.com>
2017-05-31 20:50:57 -0400

    question object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,6 +17,49 @@ var displayQuestion = false;
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var noResponse = 10;
+var currentQuestion = 0;
+
+var questions = [{
+    question: "What does TGS stand for?",
+    choices: ["That Great Show", "The Girly Show", "The Green Show", "The Gray Shadow"],
+    correctAnswer: 2
+}, {
+    question: "Who does Liz Lemon end up marrying?",
+    choices: ["Criss Cross", "Dennis Duffy", "Carol, the Pilot", "Floyd"],
+    correctAnswer: 1
+}, {
+    question: "Who are the Stars of TGS?",
+    choices: ["Frank and Toofer", "Kenneth and Pete", "Jenna Maroney and Tracy Morgan", "Jennifer Maroney and Tracy Jordon"],
+    correctAnswer: 3
+}, {
+    question: "Who is Liz Lemon's mentor?",
+    choices: ["Jack Donaghy", "Avery Jessup", "Pete Hornberger", "Hank Hooper"],
+    correctAnswer: 1
+}, {
+    question: "Jenna gains weight while working on what musical?",
+    choices: ["The Rural Juror", "Charlie and the Chocolate Factory: The Musical", "Annie", "Mystic Pizza: The Musical"],
+    correctAnswer: 4
+}, {
+    question: "What is the name of the hit party song Tracy has a gold record for?",
+    choices: ["Ghost Party", "Bewitched", "Werewolf Bar Mitzvah", "Monster Mash"],
+    correctAnswer: 3
+}, {
+    question: "What town is Kenneth Parcell, the NBC page, from?",
+    choices: ["Green Mountain, Georgia", "Stone Mountain, Georgia", "Little Mountain, Mississippi", "Red Mountain, Mississippi"],
+    correctAnswer: 2
+}, {
+    question: "What Sport did NBC not make up during the Olympics to make people believe America won more medals?",
+    choices: ["Lawn Bowling", "Octoples Tennis", "Olympic Thetherball", "Synchronized Running"],
+    correctAnswer: 2
+}, {
+    question: "What meal does Lutz instits upon ordering for TGS's last episode?",
+    choices: ["Subway", "Gas Station Hot Dogs", "Marshmallows", "Blimpies"],
+    correctAnswer: 3
+}, {
+    question: "Who does Liz Lemon dress up as to get out of jury duty?",
+    choices: ["Princess Leia", "Wonder Woman", "Catwoman", "Batman"],
+    correctAnswer: 1    
+}];
 
 // countdown functions
 function run() {
